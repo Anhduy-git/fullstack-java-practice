6223ff74d2e7e8f5e9c5bd915f0c36051317fd65
Tran Anh Duy NGUYEN <anhduydp123@gmail.com>
2025-07-01 22:15:37 +0200

    feat: add simple hardcoded login functionnality

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = '';
+  errorMessage : string = 'Invalid Credentials'
+  invalidLogin : boolean = false;
+
+  ngOnInit() {
+    console.log('LoginComponent initialized');
+  }
+
+  handleLogin() {
+    console.log('Login attempted with:', this.username, this.password);
+    if (this.username == 'duy' && this.password == 'chau') {
+      this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+    }
+  }
 
 }
